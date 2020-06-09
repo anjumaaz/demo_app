a3999ab67537b6264a03ec413ca34f1a2059f7fe
MaazAnjum <maazmkd@gmail.com>
2020-06-10 04:20:00 +0500

    Changes in CovidSym.js file

--- src/covid19sym.js
+++ src/covid19sym.js
@@ -5,7 +5,7 @@ function Covid(props) {
         <div>
           <h1> The first symptoms  {props.fistSym} </h1>
           <h1> The second symptoms {props.secondSym} </h1>
-          <h1> The  third symptoms {props.thirdSym}</h1>
+          <h1> The third symptoms {props.thirdSym}</h1>
         </div>
     
     )
