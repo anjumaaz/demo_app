a05f7b97c1ff2bff1d80719e97ea8c6fa55a4329
MaazAnjum <maazmkd@gmail.com>
2020-06-10 04:53:11 +0500

    Changes in covid19sym.js file

--- src/covid19sym.js
+++ src/covid19sym.js
@@ -3,9 +3,9 @@ import React from 'react';
 function Covid(props) {
     return (
         <div>
-          <h1> The first symptoms  {props.fistSym} </h1>
-          <h1> The second symptoms {props.secondSym} </h1>
-          <h1> The third symptoms {props.thirdSym}</h1>
+          <h1> The first symptoms of COVID-19 {props.fistSym} </h1>
+          <h1> The second symptoms of COVID-19 {props.secondSym} </h1>
+          <h1> The third symptoms of COVID-19 {props.thirdSym}</h1>
         </div>
     
     )
